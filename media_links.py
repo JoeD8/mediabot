148c101c428942bd847037179a0083da4818b9b0
JoeD8 <149216820+JoeD8@users.noreply.github.com>
2023-11-02 23:28:00 -0500

    Create media_links.py

---
+++ media_links.py
@@ -0,0 +1,17 @@
+ACCESS_KEY = "Get_from_bot_setup_page"
+
+SYSTEM_PROMPT = "This is where your bot system prompt goes"
+
+INTRO_MESSAGE = "This is the thing your bot says when you start a new chat. It can include inline images in the format ![Image description](image url)."
+
+CHOOSING_PROMPT = "A shorter system prompt to help fw-mistral-7b do a better job of picking an option"
+
+MEDIA_LIST = [
+    ["GIF","GIF description","GIF url"],
+    ["GIF","Another GIF","GIF url"],
+    ["Image","Image description","Image url"],
+    ["Image","Another image","Image url"],
+    ["Audio","Audio description","Audio url"],
+    ["Audio","Another audio","Audio url"],
+    #You can configure as many GIFs, Images, and Audios as you want. I guess you could aslo have Video items or other things, they just need a URL.
+]
