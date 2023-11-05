61a8f8afa822bbf214cc5eab5ff0af07a46de3e7
JoeD8 <149216820+JoeD8@users.noreply.github.com>
2023-11-04 22:07:08 -0500

    Update media_links.py

--- media_links.py
+++ media_links.py
@@ -7,11 +7,11 @@ INTRO_MESSAGE = "This is the thing your bot says when you start a new chat. It c
 CHOOSING_PROMPT = "You are a bot tasked with choosing imagery for the next reply (a different bot is in charge of writing the response). Pick a number from 0 to 9."
 
 MEDIA_LIST = [
-    ["GIF","GIF description","Prompt for the language bot","GIF url"],
-    ["GIF","Another GIF","Prompt for the language bot","GIF url"],
-    ["Image","Image description","Prompt for the language bot","Image url"],
-    ["Image","Another image","Prompt for the language bot","Image url"],
-    ["Audio","Audio description","Prompt for the language bot","Audio url"],
-    ["Audio","Another audio","Prompt for the language bot","Audio url"],
+    ["GIF","Description the user sees","Prompt for the language bot","GIF url"],
+    ["GIF","Description the user sees","Prompt for the language bot","GIF url"],
+    ["Image","Description the user sees","Prompt for the language bot","Image url"],
+    ["Image","Description the user sees","Prompt for the language bot","Image url"],
+    ["Audio","Description the user sees","Prompt for the language bot","Audio url"],
+    ["Audio","Description the user sees","Prompt for the language bot","Audio url"],
     #You can configure as many GIFs, Images, and Audios as you want. I guess you could aslo have Video items or other things, they just need a URL.
 ]
